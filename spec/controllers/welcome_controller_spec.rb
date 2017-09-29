d5c82c455629822dddbffd821b59e48f4e2a96bc
Natalie Wyll <nataliewyll@yahoo.com>
2017-09-29 19:52:21 +0000

    Assignment-12 corrected FAQ to faq

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -15,10 +15,10 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
   
-  describe "GET FAQ" do
-    it "renders the FAQ template" do
-      get :FAQ
-      expect(response).to render_template("FAQ")
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
     end
   end
   
