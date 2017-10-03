69710433d1f6a1f932c01cca51e8920bbc99def8
Natalie Wyll <nataliewyll@yahoo.com>
2017-10-03 02:58:05 +0000

    Assignment-17 correct file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
     @posts = Post.all
     
     #Change the title of first and every 5th post to SPAM
-    #@posts.first.title = "SPAM"
     @posts.each_with_index { |post, index|
       post.title = "SPAM" if index % 5 == 0
     }
