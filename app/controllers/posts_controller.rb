3f84f5a44eee39cebae9ce4cb74909753d04c055
Natalie Wyll <nataliewyll@yahoo.com>
2017-10-03 02:52:34 +0000

    Assignment-17 updated post controller to change every 5th post title to SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    
+    #Change the title of first and every 5th post to SPAM
+    #@posts.first.title = "SPAM"
+    @posts.each_with_index { |post, index|
+      post.title = "SPAM" if index % 5 == 0
+    }
   end
 
   def show
