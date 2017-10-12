a6316511025eb63c0b15643345ed8e128bec0c09
Natalie Wyll <nataliewyll@yahoo.com>
2017-10-12 16:44:10 +0000

    Checkpoint-26: associates posts and users - post-controller POST create failing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :require_sign_in, except: :show
+  
   def show
     @post = Post.find(params[:id])
   end
@@ -9,12 +11,8 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
     
     if @post.save
       flash[:notice] = "Posts was saved."
@@ -31,8 +29,7 @@ class PostsController < ApplicationController
   
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
     
     if @post.save
       flash[:notice] = "Posts was updated."
@@ -53,6 +50,11 @@ class PostsController < ApplicationController
       flash.now[:alert] = "There was an error deleting the post."
       render :show
     end
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
     
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,30 @@
 require 'random_data'
 
+5.times do 
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+    )
+end
+users = User.all
+  
 15.times do
   Topic.create!(
-    name: RandomData.random_sentence,
+    name:        RandomData.random_sentence,
     description: RandomData.random_paragraph
     )
 end
-
 topics = Topic.all
 
 50.times do
     Post.create!(
-      topic: topics.sample,
-      title: RandomData.random_sentence,
-      body: RandomData.random_paragraph
+      user:   users.sample,
+      topic:  topics.sample,
+      title:  RandomData.random_sentence,
+      body:   RandomData.random_paragraph
     )
 end
-
 posts = Post.all
 
 100.times do
@@ -26,7 +34,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email:    "nataliewyll@yahoo.com",
+  password: "password"
+  )
+
 puts "Seeds finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
