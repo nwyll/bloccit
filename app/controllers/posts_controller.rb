890b00baeb861def431d6aa9dea7c323daeb7fca
Natalie Wyll <nataliewyll@yahoo.com>
2017-10-13 16:55:36 +0000

    Checkpoint-27: Added user authorizations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
+  before_action :authorize_user, except: [:show, :new, :create]
   
   def show
     @post = Post.find(params[:id])
@@ -13,6 +14,7 @@ class PostsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.build(post_params)
+    @post.user = current_user
     
     if @post.save
       flash[:notice] = "Posts was saved."
@@ -35,7 +37,7 @@ class PostsController < ApplicationController
       flash[:notice] = "Posts was updated."
       redirect_to [@post.topic, @post]
     else
-      flash.now[:alert] = "There was an error saving th post. Please try again."
+      flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
     end
   end
@@ -55,6 +57,15 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+  
+  def authorize_user
+    post = Post.find(params[:id])
+    
+    unless current_user == post.user || current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
   end
     
 end
